8d556dc9a9460cc7e1bd338548e4a559087e2ed7
Kesav-Bobba <bobbak@oregonstate.edu>
2022-05-05 18:46:46 -0700

    changed

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -75,5 +75,6 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+        
     }
 }
